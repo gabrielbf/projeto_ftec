63c2255cfa13c8942dda63b0dd45129615dff924
gabrielbf <gabrielbp+github@gmail.com>
2022-06-12 22:38:19 -0300

    up e down scripts para tabela post

---
+++ internal/infra/database/migrations/000009_create_table_post.up.sql
@@ -0,0 +1,16 @@
+create table if not exists post (
+	id bigserial NOT NULL,
+	--reference_uuid uuid  UNIQUE NOT NULL,
+	description varchar NOT NULL,
+	pet_id bigserial NOT NULL,
+	status varchar NOT NULL,
+	created_at timestamptz NOT NULL,
+	updated_at timestamptz NOT NULL,
+	deleted_at timestamptz NULL DEFAULT NULL,
+
+  CONSTRAINT primary_key PRIMARY KEY (id)
+	CONSTRAINT fk_pet
+		FOREIGN KEY(pet_id))
+			REFERENCES pet(id)
+			ON DELETE SET NULL
+);
