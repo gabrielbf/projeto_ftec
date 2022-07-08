c9dc54160b8ba8519a07171e2add60054ab5144a
Lucas Montenegro <lucas.montenegro@bavabank.com>
2022-07-08 14:09:40 -0300

    fix: migrations

--- internal/infra/database/migrations/000001_create_table_account.up.sql
+++ internal/infra/database/migrations/000001_create_table_account.up.sql
@@ -1,13 +1,9 @@
 create table if not exists account (
-	id bigserial NOT NULL,
-	--reference_uuid uuid  UNIQUE NOT NULL,
+	id bigserial PRIMARY KEY NOT NULL,
 	email varchar NOT NULL,
 	password varchar NOT NULL,
 	status varchar NOT NULL,
-	type varchar NOT NULL
 	created_at timestamptz NOT NULL,
 	updated_at timestamptz NOT NULL,
-	deleted_at timestamptz NULL DEFAULT NULL,
-
-  CONSTRAINT primary_key PRIMARY KEY (id)
+	deleted_at timestamptz NULL DEFAULT NULL
 );
